892bed6653b2d01926d7d573191a35354e25594f
Long <zz7zz7zz.zz@hotmail.com>
2022-10-12 18:28:03 +0800

    条件编译:服务动态配置的处理 4

--- shell/windows/build_pre_service.bat
+++ shell/windows/build_pre_service.bat
@@ -4,7 +4,7 @@ set config_dart_Dir=%cd%
 
 echo product = %config_dart_Dir%
 cd ..\config\
-java -jar plugin_flutter_flavor.jar -e debug -c gp  -p moyu -y ./component_config.yaml -fdp ../../lib/features_core/com/module/service/service_manager.g.dart -libPath ../../lib -v true
+java -jar plugin_flutter_flavor.jar -e debug -c gp  -p moyu -y ./component_config.yaml -fdp ../../lib/features_core/com/module/service/service_manager.g.dart -rootPath ../.. -v true
 cd %config_dart_Dir%
 
 echo ----- app service end -----
